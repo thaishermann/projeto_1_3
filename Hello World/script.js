9c9ddf0c859aa335dbf0f7099f565b67fc790848
Thais Hermann <thais.hermann@escola.pr.gov.br>
2025-04-14 09:05:21 -0300

    Add files via upload

--- Hello World/script.js
+++ Hello World/script.js
@@ -1,3 +1,12 @@
-{
-    "liveServer.settings.port"; 5502
+let nome_usuario="";
+let element=document.querySelector("nome-usuario");
+
+while (nome_usuario = = " ") {
+    nome_usuario = prompt (Qual é o seu nome?);
+}
+if (nome_usuario = = null){
+    elemento.textContent = "Seja muito bem-vindo!"   
+}
+else {
+    element.textContent = "nome-usuario"
 }
